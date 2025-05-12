82cd711d663cb5213061018a351ce28fb6735c80
sidipraptama <sidiaurelius@gmail.com>
2025-05-12 17:11:49 +0700

    fix: load env

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from app.api.routes import similarity, patents
+from app.config import settings
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {
+        "message": "Hello, FastAPI!",
+        "app_name": settings.APP_NAME
+    }
 
 app.include_router(similarity.router, prefix="/api")
 app.include_router(patents.router, prefix="/api")
