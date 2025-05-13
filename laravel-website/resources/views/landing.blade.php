ceac4af0c4c9ea5a7c863ecafd05ae84730524a5
sidipraptama <sidiaurelius@gmail.com>
2025-05-13 14:50:55 +0700

    feat: new dist

--- laravel-website/resources/views/landing.blade.php
+++ laravel-website/resources/views/landing.blade.php
@@ -7,14 +7,12 @@
     <title>Landing Page</title>
     <!-- Mengarah ke file CSS yang benar -->
     <link href="{{ asset('react/dist/assets/index.css') }}" rel="stylesheet">
-    <link href="{{ asset('react/dist/assets/index-1.css') }}" rel="stylesheet">
 </head>
 
 <body>
     <div id="root"></div> <!-- React akan di-mount di sini -->
     <!-- Mengarah ke file JS yang benar -->
     <script src="{{ asset('react/dist/assets/index.js') }}"></script>
-    <script src="{{ asset('react/dist/assets/index-1.js') }}"></script>
 </body>
 
 </html>
